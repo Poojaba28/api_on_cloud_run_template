66cbdce72d87f28698a31909a0c8824da69f80fe
Poojaba28 <poojaba.j@dataslush.com>
2025-04-16 14:47:54 +0530

    commit

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from .config import APP_NAME
+
+router = APIRouter()
+
+@router.get("/health")
+def health():
+    return {"status": "ok", "app": APP_NAME}
+
+@router.post("/echo")
+def echo(data: dict):
+    return {"you_sent": data}
